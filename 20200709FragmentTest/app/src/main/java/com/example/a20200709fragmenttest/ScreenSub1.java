6531155e6c9ab4ee59c60278146182195769fb72
kss7545 <kss7545@naver.com>
2020-07-10 09:20:36 +0900

    Fragment1 이벤트 등록

--- 20200709FragmentTest/app/src/main/java/com/example/a20200709fragmenttest/ScreenSub1.java
+++ 20200709FragmentTest/app/src/main/java/com/example/a20200709fragmenttest/ScreenSub1.java
@@ -2,7 +2,9 @@ package com.example.a20200709fragmenttest;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -76,6 +78,56 @@ public class ScreenSub1 extends Fragment {
             public void onClick(View view) {
                 Intent intent = new Intent(mainActivity,Sub2Activity.class);
                 startActivity(intent);
+            }
+        });
+
+        rdoCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendarView.setVisibility(View.VISIBLE);
+                timePicker.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        rdoTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timePicker.setVisibility(View.VISIBLE);
+                calendarView.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //크로노미터 시간을 시스템 리얼타임 시간으로 설정
+                chronometer.setBase(SystemClock.elapsedRealtime());
+                chronometer.start();
+                chronometer.setTextColor(Color.RED);
+            }
+        });
+
+        btnEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //1.크로노미터 정지
+                chronometer.stop();
+                //2.설정된 시간 분을 가져온다.
+                int hour = timePicker.getHour();
+                int minute = timePicker.getMinute();
+                //3.설정된 년 월 일을 가져온다.
+                String str = year + "년" + month + "월" + day + "일" + " " + hour + "시" + minute + "분" + "예약됨";
+                tvEnd.setText(str);
+
+            }
+        });
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int y, int m, int d) {
+                year = y;
+                month = m+1;
+                day = d;
             }
         });
 
